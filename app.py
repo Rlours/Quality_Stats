b96d863259453a0a3b55b55ed294b8ef79ab59d2
Rlours <rachitwd@gmail.com>
2026-09-05 18:03:43 +0530

    Update Quality Stat page title

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit as st
 # =========================================================
 
 st.set_page_config(
-    page_title="Quality Engineering Toolkit",
+    page_title="Quality Stat",
     page_icon="📊",
     layout="wide",
     initial_sidebar_state="expanded"
